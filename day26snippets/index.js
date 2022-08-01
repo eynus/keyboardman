91bcd47f5e95ef351af34bfdc8f48acaa0de0313
eynus <736077386@qq.com>
2022-08-01 20:27:48 +0800

    gc

---
+++ day26snippets/index.js
@@ -0,0 +1,23 @@
+//imp
+import gs from 'fs';
+//ime
+import * as alias from 'module';
+//imd
+import { sdsd } from 'kk';
+//anfn
+(params) => {
+
+}
+//fin
+for (const item in object) {
+
+}
+//con
+constructor(params) {
+
+}
+//fn
+fn
+function name (params) {
+
+} 
